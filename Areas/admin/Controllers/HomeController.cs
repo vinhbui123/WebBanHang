ccec6ad4f8fcf3a1efc83ae8a956b63d7eebe425
vinhbui123 <167746847+vinhbui123@users.noreply.github.com>
2024-11-27 20:10:24 +0700

    Add files via upload

---
+++ Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebBanHang.Areas.admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebBanHang.Models;
+
+public partial class Store
+{
+    public int StoreId { get; set; }
+
+    public string StoreName { get; set; } = null!;
+
+    public string StoreAddress { get; set; } = null!;
+
+    public string? Thumb { get; set; }
+
+    public virtual ICollection<Stock> Stocks { get; set; } = new List<Stock>();
+}
