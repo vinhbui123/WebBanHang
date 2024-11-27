4d0ea2e52d0a8f9130de67b825cde50f9d932fed
Ngan22130178 <146084366+Ngan22130178@users.noreply.github.com>
2024-11-27 14:09:20 +0700

    Update Program.cs

--- WebBanHang/WebBanHang/Program.cs
+++ WebBanHang/WebBanHang/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Thêm DbContext vào dịch vụ
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -32,4 +40,31 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Thêm đoạn mã để đọc và thực thi script SQL từ file database.sql
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<MyDbContext>();
+    context.Database.EnsureCreated();
+
+    // Đọc nội dung file SQL
+    var sql = File.ReadAllText("database.sql");
+    context.Database.ExecuteSqlRaw(sql);
+}
+
 app.Run();
+
+public class MyDbContext : DbContext
+{
+    public DbSet<MyEntity> MyEntities { get; set; }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+}
+
+public class MyEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
