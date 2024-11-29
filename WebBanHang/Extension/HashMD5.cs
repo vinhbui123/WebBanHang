cc59b306d79b9ed664dace974f9b7d83dca87337
vinhbui123 <167746847+vinhbui123@users.noreply.github.com>
2024-11-29 11:16:28 +0700

    update

---
+++ WebBanHang/Extension/HashMD5.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WebBanHang.Extension
+{
+	public static class HashHelper
+	{
+		public static string ToMD5(this string str)
+		{
+			using (MD5 md5 = MD5.Create()) // Proper disposal
+			{
+				byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+				StringBuilder sb = new StringBuilder();
+				foreach (byte b in hash)
+					sb.Append(b.ToString("x2")); // More concise formatting
+				return sb.ToString();
+			}
+		}
+	}
+}
---
+++ WebBanHang/ModelViews/LoginVm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanHang.ModelViews
+{
+	public class LoginVm
+	{
+		[MaxLength(100)]
+		[Required(ErrorMessage ="Input Email")]
+		[Display(Name = "Email")]
+		public required string UserName { get; set; }
+
+		[Display(Name = "Password")]
+		[Required(ErrorMessage = "Input Password")]
+		[MinLength(5, ErrorMessage = "input minimum 5 characters")]
+		public required string Password { get; set; }
+	}
+}
